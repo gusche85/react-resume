fc69fdd7fd371475e26bbf619f3a617cb491a128
gusche85 <gishjode@gmail.com>
2024-02-07 16:38:05 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import './App.css'
+//import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
+function Introduction() {
+  return (
+    <main>
+      <div className='container-fluid m-2 p-5 border-dark'>
+      <h1 className='fw-bold'>Giuseppina Sherry Sayan</h1>
+        <p>
+          <i class="bi-envelope p-3"></i><a href="#" class="link-primary">gishjode@gmail.com</a><br></br>
+          <i class="bi-github p-3"></i><a href="#" class="link-primary">https://github.com/gusche85</a>
+        </p>
+        <hr></hr>
+        <Skills />
+        <Education />
+        <Experience />
+      </div>
+      
+    </main>
+  )
+}
+
+export function Skills() {
+  return (
+    <>
+      <div className='p-3 mb-3 bg-info bg-opacity-10 border border-info rounded'>
+    <h3>Skills:</h3></div>
+      <p>
+       JavaScript, HTML, CSS, React.js, Firebase, Express.js, JSON, Application Programming Interfaces (API), Information Technology
+      </p>
+      
+   </>
+  )
+}
+
+export function Education() {
+  return (
+    <>
+      <div className='p-3 mb-3 bg-info bg-opacity-10 border border-info rounded'>
+    <h3>Education:</h3></div>
+      <ul>
+        <li>Universiti Tun Hussein Onn</li>
+        
+        <li>Sigma School</li>
+      </ul>
+      
+   </>
+  )
+}
+
+
+export function Experience() {
+  return (
+    <>
+    <div className='p-3 mb-3 bg-info bg-opacity-10 border border-info rounded'><h3>Experience</h3></div>
+      <ul>
+        <li>Universiti Tun Hussein Onn</li>
+        <li>Sigma School</li>
+      </ul>
+      
+   </>
+  )
+}
+
+
+export default Introduction;
